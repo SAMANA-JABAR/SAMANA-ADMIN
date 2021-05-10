7764812e2a7518d13409cbcac06054685f32a0be
Fadhlan Hadaina <freehatin55@gmail.com>
2021-05-11 03:16:35 +0700

    Adding login layout

---
+++ app/src/main/java/com/user/fadhlanhadaina/samana_admin/ui/activities/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.user.fadhlanhadaina.samana_admin.ui.activities.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.user.fadhlanhadaina.samana_admin.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
