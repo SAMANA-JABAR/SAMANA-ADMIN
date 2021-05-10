09f7db90c19df1808f78d80fee378335d61d3ca6
Fadhlan Hadaina <freehatin55@gmail.com>
2021-05-11 02:29:15 +0700

    first commit

---
+++ app/src/main/java/com/user/fadhlanhadaina/samana_admin/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.user.fadhlanhadaina.samana_admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/SAMANA-JABAR/SAMANA-ADMIN.git
+git push -u origin main
