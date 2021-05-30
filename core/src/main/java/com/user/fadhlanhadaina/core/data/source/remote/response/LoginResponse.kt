f8a35ef6f79958ceeabc1a39e3b587f9f43035bb
Fadhlan Hadaina <freehatin55@gmail.com>
2021-05-30 18:21:14 +0700

    add SerializeName @LoginResponse for "name"

--- core/src/main/java/com/user/fadhlanhadaina/core/data/source/remote/response/LoginResponse.kt
+++ core/src/main/java/com/user/fadhlanhadaina/core/data/source/remote/response/LoginResponse.kt
@@ -1,6 +1,9 @@
 package com.user.fadhlanhadaina.core.data.source.remote.response
 
+import com.google.gson.annotations.SerializedName
+
 data class LoginResponse(
+    @SerializedName("nama")
     val name: String,
     val username: String,
     val email: String,
